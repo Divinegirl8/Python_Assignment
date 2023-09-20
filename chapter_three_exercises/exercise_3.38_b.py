c6bd442478376ad37c1fc869507d222338e93a6a
Ayinlade Sikiru <ayinladesikiru@gmail.com>
2023-09-20 09:06:56 +0100

    Update exercise_3.38_b.py

--- chapter_three_exercises/exercise_3.38_b.py
+++ chapter_three_exercises/exercise_3.38_b.py
@@ -3,5 +3,7 @@ import statistics
 number = [9, 11, 22, 34, 17, 22, 34, 22, 40, 34]
 sorted(number)
 
+# pls no built-in function
+
 print(
     f'The mean of the number is {statistics.mean(number):.1f}\nThe mode of the number is {statistics.mode(number)}\nThe median of the number is {statistics.median(number)}')
